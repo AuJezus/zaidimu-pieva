0375e180d175dee11de9f20759a80a144368e26b
aujezus <augispay@gmail.com>
2024-05-23 08:08:05 +0300

    Add reveal animation to section

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,20 +1,27 @@
+"use client";
+
 import { cn } from "~/lib/utils";
+import { type HTMLMotionProps, motion } from "framer-motion";
 
 function Section({
   className,
   children,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: HTMLMotionProps<"section">) {
   return (
-    <section
+    <motion.section
       className={cn(
         "relative z-10 mx-auto mb-16 max-w-[1200px] px-4 lg:px-6 xl:px-0",
         className,
       )}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
       {...props}
     >
       {children}
-    </section>
+    </motion.section>
   );
 }
 
